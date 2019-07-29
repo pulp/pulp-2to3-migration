56ea0ed75e34da968e15e9299e3cb7360ac9dfd1
Tatiana Tereshchenko <ttereshc@redhat.com>
2019-07-29 14:45:13 +0200

    Migrate Pulp 2 ISO content into Pulp 3
    Create hard links (of copy if not possible) and create Artifacts,
    ContentArtifacts and Content in pulp 3.
    
    closes #3810
    https://pulp.plan.io/issues/3810
    
    Required PR: https://github.com/pulp/pulpcore/pull/254/

---
+++ pulp_2to3_migrate/app/models/base.py
@@ -0,0 +1,99 @@
+from django.db import models
+
+from pulpcore.plugin.models import (
+    Content,
+    Model
+)
+
+
+class MigrationPlan(Model):
+    """
+    Migration Plans that have been created and maybe even run.
+
+    Fields:
+        plan (models.TextField): The migration plan in the JSON format
+    """
+    plan = models.TextField()
+    # TODO: convert to JSONField
+
+
+class Pulp2Content(Model):
+    """
+    General info about Pulp 2 content.
+
+    Pulp3 plugin models should create a Foreign key to this model.
+
+    Fields:
+        pulp2_id (models.UUIDField): Content ID in Pulp 2
+        pulp2_content_type_id (models.CharField): Content type in Pulp 2
+        pulp2_last_updated (models.PositiveIntegerField): Content creation or update time in Pulp 2
+        pulp2_storage_path (models.TextField): Content storage path on Pulp 2 system
+        downloaded (models.BooleanField): Flag to identify if content is on a filesystem or not
+
+    Relations:
+        pulp3_content (models.ForeignKey): Pulp 3 content which Pulp 2 content was migrated to
+    """
+    pulp2_id = models.CharField(max_length=255)
+    pulp2_content_type_id = models.CharField(max_length=255)
+    pulp2_last_updated = models.PositiveIntegerField()
+    pulp2_storage_path = models.TextField()
+    downloaded = models.BooleanField(default=True)
+    pulp3_content = models.ForeignKey(Content, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        unique_together = ('pulp2_id', 'pulp2_content_type_id')
+
+
+class Pulp2to3Content(Model):
+    """
+    Pulp 2to3 detail content model to store pulp 2 content details for Pulp 3 content creation.
+    """
+    pulp2content = models.ForeignKey(Pulp2Content,
+                                     related_name='detail_model',
+                                     on_delete=models.CASCADE)
+    type = '<your pulp 2 content type>'
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    async def pre_migrate_content_detail(cls, content_batch):
+        """
+        Pre-migrate Pulp 2 content with all the fields needed to create a Pulp 3 Content
+
+        Args:
+             content_batch(list of Pulp2Content): pre-migrated generic data for Pulp 2 content.
+
+        Example for ISO content:
+        >>> pulp2_map = {pulp2content.pulp2_id: pulp2content for pulp2content in content_batch}
+        >>> pulp2_ids = pulp2_id_obj_map.keys()
+        >>> pulp2_iso_content_batch = ISO.objects.filter(id__in=pulp2_ids)
+        >>> pulp2iso_to_save = [Pulp2ISO(name=iso.name,
+        >>>                              checksum=iso.checksum,
+        >>>                              size=iso.size,
+        >>>                              pulp2content=pulp2_map[iso.id])
+        >>>                     for iso in pulp2_iso_content_batch]
+        >>> cls.objects.bulk_create(pulp2iso_to_save, ignore_conflicts=True)
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    async def migrate_content_to_pulp3(cls):
+        """
+        Migrate pre-migrated Pulp 2 content.
+
+        Create a DeclatativeContentMigration pipeline here and instantiate it with your first stage.
+        Here the default implementation of the first stage is used:
+        >>> first_stage = ContentMigrationFirstStage(cls)
+        >>> dv = DeclarativeContentMigration(first_stage=first_stage)
+        >>> await dv.create()
+        """
+        raise NotImplementedError()
+
+    def create_pulp3_content(self):
+        """
+        Create a Pulp 3 detail Content unit for saving it later in a bulk operation.
+
+        Return an unsaved Pulp 3 Content
+        """
+        raise NotImplementedError()
---
+++ pulp_2to3_migrate/app/plugin/iso/pulp3/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+
+from pulp_2to3_migrate.app.models import Pulp2to3Content
+from pulp_2to3_migrate.app.plugin.api import (
+    ContentMigrationFirstStage,
+    DeclarativeContentMigration,
+)
+from pulp_2to3_migrate.app.plugin.iso.pulp2.models import ISO
+
+from pulp_file.app.models import FileContent
+
+
+class Pulp2ISO(Pulp2to3Content):
+    """
+    Pulp 2to3 detail content model to store pulp 2 ISO content details for Pulp 3 content creation.
+    """
+    name = models.CharField(max_length=255)  # TODO: what to do if it's longer in Pulp 2?
+    checksum = models.CharField(max_length=64)
+    size = models.BigIntegerField()
+
+    type = 'iso'
+    checksum_type = 'sha256'
+
+    class Meta:
+        unique_together = ('name', 'checksum', 'size', 'pulp2content')
+
+    @property
+    def expected_digests(self):
+        return {self.checksum_type: self.checksum}
+
+    @property
+    def expected_size(self):
+        return self.size
+
+    @property
+    def relative_path_for_content_artifact(self):
+        return self.name
+
+    @classmethod
+    async def pre_migrate_content_detail(cls, content_batch):
+        """
+        Pre-migrate Pulp 2 ISO content with all the fields needed to create a Pulp 3 FileContent
+
+        Args:
+             content_batch(list of Pulp2Content): pre-migrated generic data for Pulp 2 content.
+
+        """
+        # TODO: all pulp2content objects from the batch are in memory. Concerns?
+        pulp2_id_obj_map = {pulp2content.pulp2_id: pulp2content for pulp2content in content_batch}
+        pulp2_ids = pulp2_id_obj_map.keys()
+        pulp2_iso_content_batch = ISO.objects.filter(id__in=pulp2_ids)
+        pulp2iso_to_save = [Pulp2ISO(name=iso.name,
+                                     checksum=iso.checksum,
+                                     size=iso.size,
+                                     pulp2content=pulp2_id_obj_map[iso.id])
+                            for iso in pulp2_iso_content_batch]
+        cls.objects.bulk_create(pulp2iso_to_save, ignore_conflicts=True)
+
+    @classmethod
+    async def migrate_content_to_pulp3(cls):
+        """
+        Migrate pre-migrated Pulp 2 ISO content.
+        """
+        first_stage = ContentMigrationFirstStage(cls)
+        dv = DeclarativeContentMigration(first_stage=first_stage)
+        await dv.create()
+
+    def create_pulp3_content(self):
+        """
+        Create a Pulp 3 FileContent unit for saving it later in a bulk operation.
+        """
+        return FileContent(relative_path=self.name,
+                           digest=self.checksum)
