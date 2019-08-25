3a62b6dabe92e47fa97dcd13805ccd3b5d6c0e48
Tatiana Tereshchenko <ttereshc@redhat.com>
2019-08-25 22:02:51 +0200

    Add models for pre0migrating pulp2 repository related data
    closes #5306
    https://pulp.plan.io/issues/5306

--- pulp_2to3_migrate/app/models/__init__.py
+++ pulp_2to3_migrate/app/models/__init__.py
@@ -1,8 +1,13 @@
-from .base import (  # noqa
-    MigrationPlan,
+from .base import MigrationPlan  # noqa
+from .content import (  # noqa
     Pulp2Content,
     Pulp2to3Content,
 )
-
+from .repository import (  # noqa
+    Pulp2Distributor,
+    Pulp2Importer,
+    Pulp2RepoContent,
+    Pulp2Repository,
+)
 # import all pulp_2to3 detail plugin models here
 from pulp_2to3_migrate.app.plugin.iso.pulp3.models import Pulp2ISO  # noqa
