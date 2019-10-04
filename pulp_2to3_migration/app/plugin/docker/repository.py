f2021a5fdacb4944824276f5087b85635621a629
Ina Panova <ipanova@redhat.com>
2019-10-04 13:41:20 +0200

    Migrate Docker Importers.
    [noissue]

--- pulp_2to3_migration/app/plugin/docker/repository.py
+++ pulp_2to3_migration/app/plugin/docker/repository.py
@@ -1,12 +1,13 @@
 from pulp_2to3_migration.app.plugin.api import Pulp2to3Importer
 
-from pulp_file.app.models import FileRemote
+from pulp_docker.app.models import DockerRemote
 
 
-class IsoImporter(Pulp2to3Importer):
+class DockerImporter(Pulp2to3Importer):
     """
-    Interface to migrate Pulp 2 ISO importer
+    Interface to migrate Pulp 2 Docker importer
     """
+
     @classmethod
     async def migrate_to_pulp3(cls, pulp2importer):
         """
@@ -16,9 +17,12 @@ class IsoImporter(Pulp2to3Importer):
             pulp2importer(Pulp2Importer): Pre-migrated pulp2 importer to migrate
 
         Return:
-            remote(FileRemote): FileRemote in Pulp3
+            remote(DockerRemote): DockerRemote in Pulp3
             created(bool): True if Remote has just been created; False if Remote is an existing one
         """
         pulp2_config = pulp2importer.pulp2_config
         base_config = cls.parse_base_config(pulp2importer, pulp2_config)
-        return FileRemote.objects.update_or_create(**base_config)
+        # what to do if there is no upstream name?
+        base_config['upstream_name'] = pulp2_config.get('upstream_name', '')
+        base_config['whitelist_tags'] = pulp2_config.get('tags')
+        return DockerRemote.objects.update_or_create(**base_config)
--- pulp_2to3_migration/app/plugin/repository.py
+++ pulp_2to3_migration/app/plugin/repository.py
@@ -1,3 +1,8 @@
+from urllib.parse import urlparse
+
+from pulp_2to3_migration.app.constants import PULP_2TO3_POLICIES
+
+
 class Pulp2to3Importer:
     """
     Pulp 2to3 importer migration interface.
@@ -6,6 +11,47 @@ class Pulp2to3Importer:
     """
     class Meta:
         abstract = True
+
+    @staticmethod
+    def parse_base_config(pulp2importer, pulp2_config):
+        """
+        Parse and return basic config.
+        """
+        base_config = {}
+        proxy_url, credentials, host = None, None, None
+        pulp2_proxy_host = pulp2_config.get('proxy_host')
+        pulp2_proxy_port = pulp2_config.get('proxy_port')
+        pulp2_proxy_username = pulp2_config.get('proxy_username')
+        pulp2_proxy_password = pulp2_config.get('proxy_password')
+        if pulp2_proxy_username:
+            credentials = '{}:{}'.format(pulp2_proxy_username, pulp2_proxy_password)
+        if pulp2_proxy_host:
+            parsed_url = urlparse(pulp2_proxy_host)
+            scheme = parsed_url.scheme
+            host = parsed_url.hostname
+            if pulp2_proxy_port:
+                host += ':{}'.format(pulp2_proxy_port)
+            if credentials:
+                proxy_url = '{}://{}@{}'.format(scheme, credentials, host)
+            else:
+                proxy_url = '{}://{}'.format(scheme, host)
+        # remote name in Pulp 3 is limited to 255 characters
+        remote_name = '{}-{}'.format(pulp2importer.pulp2_object_id,
+                                     pulp2importer.pulp2_repository.pulp2_repo_id)[:255]
+        base_config['proxy_url'] = proxy_url
+        base_config['name'] = remote_name
+        base_config['url'] = pulp2_config.get('feed', '')  # what to do if there is no feed?
+        base_config['ssl_ca_certificate'] = pulp2_config.get('ssl_ca_cert')
+        base_config['ssl_client_certificate'] = pulp2_config.get('ssl_client_cert')
+        base_config['ssl_client_key'] = pulp2_config.get('ssl_client_key')
+        # True by default?
+        base_config['ssl_validation'] = pulp2_config.get('ssl_validation', True)
+        base_config['username'] = pulp2_config.get('basic_auth_username')
+        base_config['password'] = pulp2_config.get('basic_auth_password')
+        base_config['download_concurrency'] = pulp2_config.get('max_downloads') or 20
+        policy = PULP_2TO3_POLICIES.get(pulp2_config.get('download_policy', 'immediate'))
+        base_config['policy'] = policy
+        return base_config
 
     @classmethod
     async def migrate_to_pulp3(cls, pulp2importer):
