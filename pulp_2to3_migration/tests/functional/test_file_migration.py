d5f89f5a95e9d1171ab917e71c2e7d187f94a7d4
Dennis Kliban <dkliban@redhat.com>
2019-12-20 13:54:38 -0500

    Makes names of api clients more clear.
    This patch also updates docstring and rearranges the imports.
    
    [noissue]

--- pulp_2to3_migration/tests/functional/test_file_migration.py
+++ pulp_2to3_migration/tests/functional/test_file_migration.py
@@ -1,14 +1,21 @@
 import socket
 from time import sleep
 import unittest
-from pulpcore.client.pulpcore import (ApiClient as CoreApiClient, Configuration,
-                                      TasksApi)
-from pulpcore.client.pulp_file import (ApiClient as FileApiClient,
-                                       ContentFilesApi,
-                                       RepositoriesFileApi,
-                                       RepositoriesFileVersionsApi)
-from pulpcore.client.pulp_2to3_migration import (ApiClient as MigrationApiClient,
-                                                 MigrationPlansApi)
+from pulpcore.client.pulpcore import (
+    ApiClient as CoreApiClient,
+    Configuration,
+    TasksApi
+)
+from pulpcore.client.pulp_file import (
+    ApiClient as FileApiClient,
+    ContentFilesApi,
+    RepositoriesFileApi,
+    RepositoriesFileVersionsApi
+)
+from pulpcore.client.pulp_2to3_migration import (
+    ApiClient as MigrationApiClient,
+    MigrationPlansApi
+)
 from pulp_2to3_migration.pulp2.base import Repository as Pulp2Repository, RepositoryContentUnit
 from pulp_2to3_migration.pulp2.connection import initialize
 
@@ -64,27 +71,22 @@ class TestMigrationPlan(unittest.TestCase):
         migration_client = MigrationApiClient(configuration)
 
         # Create api clients for all resource types
-        # artifacts = ArtifactsApi(core_client)
-        cls.file_repositories = RepositoriesFileApi(file_client)
-        cls.file_repo_versions = RepositoriesFileVersionsApi(file_client)
-        cls.file_content = ContentFilesApi(file_client)
-        # filedistributions = DistributionsFileApi(core_client)
-        # filepublications = PublicationsFileApi(file_client)
-        # fileremotes = RemotesFileApi(file_client)
-        cls.tasks = TasksApi(core_client)
-        # uploads = UploadsApi(core_client)
-        cls.migration_plans = MigrationPlansApi(migration_client)
+        cls.file_repo_api = RepositoriesFileApi(file_client)
+        cls.file_repo_versions_api = RepositoriesFileVersionsApi(file_client)
+        cls.file_content_api = ContentFilesApi(file_client)
+        cls.tasks_api = TasksApi(core_client)
+        cls.migration_plans_api = MigrationPlansApi(migration_client)
 
     def test_migrate_iso_repositories(self):
-        """Test that a valid Migration Plan can be created."""
+        """Test that a Migration Plan to migrate ISO content executes correctly."""
         migration_plan = '{"plugins": [{"type": "iso"}]}'
-        mp = self.migration_plans.create({'plan': migration_plan})
-        mp_run_response = self.migration_plans.run(mp.pulp_href, data={})
-        task = monitor_task(self.tasks, mp_run_response.task)
+        mp = self.migration_plans_api.create({'plan': migration_plan})
+        mp_run_response = self.migration_plans_api.run(mp.pulp_href, data={})
+        task = monitor_task(self.tasks_api, mp_run_response.task)
         self.assertEqual(task.state, "completed")
         pulp2repositories = Pulp2Repository.objects.all()
         for pulp2repo in pulp2repositories:
-            pulp3repos = self.file_repositories.list(name=pulp2repo.repo_id)
+            pulp3repos = self.file_repo_api.list(name=pulp2repo.repo_id)
             repo_href = pulp3repos.results[0].pulp_href
             self.failIf(not pulp3repos.results,
                         "Missing a Pulp 3 repository for Pulp 2 "
@@ -93,7 +95,7 @@ class TestMigrationPlan(unittest.TestCase):
             self.assertEqual(pulp2repo.repo_id, pulp3repos.results[0].name)
             # Assert that there is a Repository Version with the same number of content units as
             # associated with the repository in Pulp 2.
-            repo_version_href = self.file_repo_versions.list(repo_href).results[0].pulp_href
+            repo_version_href = self.file_repo_versions_api.list(repo_href).results[0].pulp_href
             pulp2_repo_content = RepositoryContentUnit.objects.filter(repo_id=pulp2repo.repo_id)
-            repo_version_content = self.file_content.list(repository_version=repo_version_href)
+            repo_version_content = self.file_content_api.list(repository_version=repo_version_href)
             self.assertEqual(pulp2_repo_content.count(), repo_version_content.count)
