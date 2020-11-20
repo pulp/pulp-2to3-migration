71f0f735affee11f5bae1253ba701f7ae045b53c
Daniel Alley <dalley@redhat.com>
2020-11-20 11:57:34 -0500

    Add Github Actions CI
    [noissue]

---
+++ .github/workflows/scripts/post_before_install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -mveuo pipefail
+
+export MONGODB_IP=$(ip address show dev docker0 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
+sed -i "s/'seeds': 'localhost:27017'/'seeds': '$MONGODB_IP:27017'/g" $GITHUB_WORKSPACE/pulp_2to3_migration/app/settings.py
+sed -i "s/'username': ''/'username': 'ci_cd'/g" $GITHUB_WORKSPACE/pulp_2to3_migration/app/settings.py
+sed -i "s/'password': ''/'password': 'ci_cd'/g" $GITHUB_WORKSPACE/pulp_2to3_migration/app/settings.py
