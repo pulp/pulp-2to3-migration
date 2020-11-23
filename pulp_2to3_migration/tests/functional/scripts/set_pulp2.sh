02257ff5b8e90e1c3850248f03f466d35a0df3e7
Tatiana Tereshchenko <ttereshc@redhat.com>
2020-11-23 18:51:25 +0100

    Add ability to apply a pulp 2 snapshot from within a test.
    closes #7903
    https://pulp.plan.io/issues/7903

---
+++ pulp_2to3_migration/tests/functional/scripts/set_pulp2.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+MONGO_HOST=$1
+ARCHIVE_NAME=$2
+
+[ ! -d "pulp-2to3-migration-test-fixtures" ] && git clone https://github.com/pulp/pulp-2to3-migration-test-fixtures
+sudo rm -rf /var/lib/pulp/content
+sudo rm -rf /var/lib/pulp/published
+cp -r pulp-2to3-migration-test-fixtures/${ARCHIVE_NAME}/var/lib/pulp/content /var/lib/pulp/
+cp -r pulp-2to3-migration-test-fixtures/${ARCHIVE_NAME}/var/lib/pulp/published /var/lib/pulp/
+
+mongo --host ${MONGO_HOST} pulp_database --eval 'db.dropDatabase();'
+mongorestore --host ${MONGO_HOST} --archive=pulp-2to3-migration-test-fixtures/${ARCHIVE_NAME}/pulp2filecontent.${ARCHIVE_NAME}.archive --drop
