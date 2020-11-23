0ec3ad8be0897fec762de276d328e225b0534ade
Tatiana Tereshchenko <ttereshc@redhat.com>
2020-11-23 18:51:25 +0100

    Add ability to apply a pulp 2 snapshot from within a test.
    closes #7903
    https://pulp.plan.io/issues/7903

--- pulp_2to3_migration/tests/functional/util.py
+++ pulp_2to3_migration/tests/functional/util.py
@@ -1,6 +1,16 @@
+import logging
+import os
+import subprocess
+
 from time import sleep
 
 from .dynaconf_config import settings
+
+from pulp_smash import cli, utils
+from pulp_smash import config as smash_config
+
+
+_logger = logging.getLogger(__name__)
 
 
 def monitor_task(tasks_api, task_href):
@@ -48,3 +58,33 @@ def get_psql_smash_cmd(sql_statement):
         'psql', '-c', sql_statement,
         f'postgresql://{user}:{password}@{host}/{dbname}'
     )
+
+
+def set_pulp2_snapshot(name):
+    """
+    Roll out a specific pulp 2 snapshot
+
+    Args:
+        name(str): directory name  of the pulp 2 snapshot
+
+    """
+    smash_cfg = smash_config.get_config()
+    smash_cli_client = cli.Client(smash_cfg)
+    pulp2_mongodb_conf = utils.get_pulp_setting(smash_cli_client, "PULP2_MONGODB")
+    mongodb_host = pulp2_mongodb_conf['seeds'].split(':')[0]
+
+    pulp2_fs_setup_script_path = '/tmp/set_pulp2.sh'
+
+    # for running tests locally
+    if smash_cfg.hosts[0].hostname == 'localhost':
+        basepath = os.path.dirname(os.path.realpath(__file__))
+        pulp2_fs_setup_script_path = os.path.join(basepath, 'scripts/set_pulp2.sh')
+
+    cmd = ('bash', pulp2_fs_setup_script_path, mongodb_host, name)
+    smash_cli_client.run(cmd, sudo=True)
+
+    # needs to be done locally otherwise auth is required because password is provided in the
+    # cleartext.
+    cmd = ('mongo', 'pulp_database', '--eval', 'db.createUser({user:"ci_cd",pwd:"ci_cd", '
+                                               'roles:["readWrite"]});')
+    subprocess.run(cmd)
