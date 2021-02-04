05df8733380f9af187369f59aaad788619b4a89a
Tatiana Tereshchenko <ttereshc@redhat.com>
2021-02-04 13:43:44 +0100

    Set pulpcore and plugin version for tests and Z releases.
    [noissue]

--- .github/workflows/scripts/before_install.sh
+++ .github/workflows/scripts/before_install.sh
@@ -88,7 +88,7 @@ sed -i -e 's/localhost:24817/pulp/g' generate.sh
 sed -i -e 's/:24817/pulp/g' generate.sh
 cd ..
 
-git clone --depth=1 https://github.com/pulp/pulpcore.git --branch master
+git clone --depth=1 https://github.com/pulp/pulpcore.git --branch 3.9
 
 cd pulpcore
 if [ -n "$PULPCORE_PR_NUMBER" ]; then
@@ -111,7 +111,7 @@ fi
 pip install --upgrade --force-reinstall ./pulp-smash
 
 
-git clone --depth=1 https://github.com/pulp/pulp_file.git --branch master
+git clone --depth=1 https://github.com/pulp/pulp_file.git --branch 1.5
 if [ -n "$PULP_FILE_PR_NUMBER" ]; then
   cd pulp_file
   git fetch --depth=1 origin pull/$PULP_FILE_PR_NUMBER/head:$PULP_FILE_PR_NUMBER
@@ -119,7 +119,7 @@ if [ -n "$PULP_FILE_PR_NUMBER" ]; then
   cd ..
 fi
 
-git clone --depth=1 https://github.com/pulp/pulp_container.git --branch master
+git clone --depth=1 https://github.com/pulp/pulp_container.git --branch 2.2
 if [ -n "$PULP_CONTAINER_PR_NUMBER" ]; then
   cd pulp_container
   git fetch --depth=1 origin pull/$PULP_CONTAINER_PR_NUMBER/head:$PULP_CONTAINER_PR_NUMBER
@@ -127,7 +127,7 @@ if [ -n "$PULP_CONTAINER_PR_NUMBER" ]; then
   cd ..
 fi
 
-git clone --depth=1 https://github.com/pulp/pulp_rpm.git --branch master
+git clone --depth=1 https://github.com/pulp/pulp_rpm.git --branch 3.8
 if [ -n "$PULP_RPM_PR_NUMBER" ]; then
   cd pulp_rpm
   git fetch --depth=1 origin pull/$PULP_RPM_PR_NUMBER/head:$PULP_RPM_PR_NUMBER
@@ -135,7 +135,7 @@ if [ -n "$PULP_RPM_PR_NUMBER" ]; then
   cd ..
 fi
 
-git clone --depth=1 https://github.com/pulp/pulp_deb.git --branch main
+git clone --depth=1 https://github.com/pulp/pulp_deb.git --branch 2.9
 if [ -n "$PULP_DEB_PR_NUMBER" ]; then
   cd pulp_deb
   git fetch --depth=1 origin pull/$PULP_DEB_PR_NUMBER/head:$PULP_DEB_PR_NUMBER
