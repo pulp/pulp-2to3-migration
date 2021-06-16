65da0f3a8a9fa6d2c22647fde1f9668da733a4d5
Dennis Kliban <dkliban@redhat.com>
2021-06-15 20:32:51 -0400

    Copy settings.py from plugin into the settings.py.j2 template
    [noissue]

--- .github/workflows/scripts/post_before_install.sh
+++ .github/workflows/scripts/post_before_install.sh
@@ -6,3 +6,4 @@ export MONGODB_IP=$(ip address show dev docker0 | grep -o "inet [0-9]*\.[0-9]*\.
 sed -i "s/'seeds': 'localhost:27017'/'seeds': '$MONGODB_IP:27017'/g" $GITHUB_WORKSPACE/pulp_2to3_migration/app/settings.py
 sed -i "s/'username': ''/'username': 'ci_cd'/g" $GITHUB_WORKSPACE/pulp_2to3_migration/app/settings.py
 sed -i "s/'password': ''/'password': 'ci_cd'/g" $GITHUB_WORKSPACE/pulp_2to3_migration/app/settings.py
+cat $GITHUB_WORKSPACE/pulp_2to3_migration/app/settings.py >> $GITHUB_WORKSPACE/.ci/ansible/settings.py.j2
