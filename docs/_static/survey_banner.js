ddb8548d1d58b2543e67901e5286aea79bc19948
Gerrod Ubben <gerrodubben@gmail.com>
2021-06-18 10:22:08 -0400

    Fix survey banner
    [noissue]

--- docs/_static/survey_banner.js
+++ docs/_static/survey_banner.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     var elem = document.createElement('div');
-    var body = document.getElementsByClassName("rst-content")[0]
-    var doc = document.getElementsByClassName("document")[0]
+    var body = document.getElementsByClassName("bodywrapper")[0]
+    var doc = document.getElementsByClassName("body")[0]
     elem.className = "admonition important"
     elem.id = "pulp-survey-banner"
     elem.innerHTML = "<p>Please take our <a href=\"https://forms.gle/C3QwT9SVncXETipu9\">survey</a> to help us improve Pulp!</p>";
