740d4115384abf0d707e65b9a4553248f46da892
Tatiana Tereshchenko <ttereshc@redhat.com>
2021-08-31 12:21:38 +0200

    Give in to the double quotes in setup.py
    [noissue]

--- setup.py
+++ setup.py
@@ -6,35 +6,35 @@ with open("requirements.txt") as requirements:
     requirements = requirements.readlines()
 
 setup(
-    name='pulp-2to3-migration',
-    version='0.14.0.dev',
-    description='Pulp 2 to Pulp 3 migration tool',
-    license='GPLv2+',
-    author='Pulp Team',
-    author_email='pulp-list@redhat.com',
-    url='http://www.pulpproject.org',
-    python_requires='>=3.8',
+    name="pulp-2to3-migration",
+    version="0.14.0.dev",
+    description="Pulp 2 to Pulp 3 migration tool",
+    license="GPLv2+",
+    author="Pulp Team",
+    author_email="pulp-list@redhat.com",
+    url="http://www.pulpproject.org",
+    python_requires=">=3.8",
     install_requires=requirements,
     include_package_data=True,
-    packages=find_packages(exclude=['tests', 'tests.*']),
+    packages=find_packages(exclude=["tests", "tests.*"]),
     classifiers=(
-        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
-        'Operating System :: POSIX :: Linux',
-        'Framework :: Django',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: POSIX :: Linux",
+        "Framework :: Django",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ),
     entry_points={
-        'pulpcore.plugin': [
-            'pulp_2to3_migration = pulp_2to3_migration:default_app_config',
+        "pulpcore.plugin": [
+            "pulp_2to3_migration = pulp_2to3_migration:default_app_config",
         ],
-        'migrators': [
-            'iso = pulp_2to3_migration.app.plugin.iso.migrator:IsoMigrator',
-            'docker = pulp_2to3_migration.app.plugin.docker.migrator:DockerMigrator',
-            'deb = pulp_2to3_migration.app.plugin.deb.migrator:DebMigrator',
-            'rpm = pulp_2to3_migration.app.plugin.rpm.migrator:RpmMigrator'
+        "migrators": [
+            "iso = pulp_2to3_migration.app.plugin.iso.migrator:IsoMigrator",
+            "docker = pulp_2to3_migration.app.plugin.docker.migrator:DockerMigrator",
+            "deb = pulp_2to3_migration.app.plugin.deb.migrator:DebMigrator",
+            "rpm = pulp_2to3_migration.app.plugin.rpm.migrator:RpmMigrator"
         ]
     }
 )
