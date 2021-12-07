c684f60732681085a051fb2e152857c9797d01c3
pulpbot <pulp-infra@redhat.com>
2021-12-07 21:23:47 +0000

    Bump to 0.11.8.dev
    [noissue]

--- docs/conf.py
+++ docs/conf.py
@@ -32,9 +32,9 @@ author = 'Pulp Team'
 
 
 # The short X.Y version
-version = "0.11.7"
+version = "0.11.8.dev"
 # The full version, including alpha/beta/rc tags
-release = "0.11.7"
+release = "0.11.8.dev"
 
 
 # -- General configuration ---------------------------------------------------
--- pulp_2to3_migration/app/__init__.py
+++ pulp_2to3_migration/app/__init__.py
@@ -6,4 +6,4 @@ class Pulp2To3MigrationPluginAppConfig(PulpPluginAppConfig):
 
     name = 'pulp_2to3_migration.app'
     label = 'pulp_2to3_migration'
-    version = '0.11.7'
+    version = '0.11.8.dev'
