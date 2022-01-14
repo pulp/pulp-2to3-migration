ade31f03794cc59ba1b7a20e1b0568872a6501c7
pulpbot <pulp-infra@redhat.com>
2022-01-14 16:33:03 +0000

    Bump to 0.11.10.dev
    [noissue]

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as requirements:
 
 setup(
     name='pulp-2to3-migration',
-    version='0.11.9',
+    version='0.11.10.dev',
     description='Pulp 2 to Pulp 3 migration tool',
     license='GPLv2+',
     author='Pulp Team',
