87273641910cd2288e6bcaf4df41a9421ea77ce4
pulpbot <pulp-infra@redhat.com>
2022-03-15 12:07:51 +0000

    Release 0.16.0 GH Issues: 491,511,8968,497,9564,484,496,483
    [noissue]

--- docs/conf.py
+++ docs/conf.py
@@ -32,9 +32,9 @@ author = 'Pulp Team'
 
 
 # The short X.Y version
-version = "0.16.0.dev"
+version = "0.16.0"
 # The full version, including alpha/beta/rc tags
-release = "0.16.0.dev"
+release = "0.16.0"
 
 
 # -- General configuration ---------------------------------------------------
