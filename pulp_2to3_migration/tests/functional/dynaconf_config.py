9889f3e52aff6b93d03be936d4c8c0d9e99f84d0
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-11-03 17:33:54 +0000

    Update 0.11 CI
    [noissue]

--- pulp_2to3_migration/tests/functional/dynaconf_config.py
+++ pulp_2to3_migration/tests/functional/dynaconf_config.py
@@ -1,3 +1,9 @@
 from dynaconf import Dynaconf
 
-settings = Dynaconf(settings_files=['func_test_settings.py', '/etc/pulp/settings.py'])
+settings = Dynaconf(
+    settings_files=[
+        "func_test_settings.py",
+        "/pulp-2to3-migration/func_test_settings.py",
+        "/etc/pulp/settings.py",
+    ]
+)
